6e7bd35237e16b61e329ebee1ba9de890fcb705d
Nicolas Guich <guichnicolas@gmail.com>
2026-04-11 20:31:14 -0700

    scrub mongo credential

--- client/src/App.js
+++ client/src/App.js
@@ -1,93 +1,82 @@
-import React from 'react';
-import LogInSignUp from './LogInSignUp';
-import './App.css';
-import GamePage from './GamePage';
+import React from "react";
+import LogInSignUp from "./LogInSignUp";
+import "./App.css";
+import GamePage from "./GamePage";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      session: false
-    }
-    this.signUp = this.signUp.bind(this)
-    this.logIn = this.logIn.bind(this)
-    this.logOut = this.logOut.bind(this)
+      session: false,
+    };
+    this.signUp = this.signUp.bind(this);
+    this.logIn = this.logIn.bind(this);
+    this.logOut = this.logOut.bind(this);
   }
   componentDidMount() {
-    let accountId = localStorage.getItem("_id")
+    let accountId = localStorage.getItem("_id");
     fetch("http://localhost:3001/session", {
       method: "POST",
       headers: {
-        'Content-Type': "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        accountId: accountId
+        accountId: accountId,
+      }),
+    })
+      .then((response) => {
+        return response.json();
       })
-    }).then(
-      response => {
-        return response.json()
-      }
-    ).then(
-      data => {
-        console.log(data)
+      .then((data) => {
         if (data.success) {
-          this.setState({session: true})
+          this.setState({ session: true });
         }
-      }
-    )
+      });
   }
   signUp(usernameInput, passwordInput, emailInput) {
     fetch("http://localhost:3001/signup", {
       method: "POST",
       headers: {
-        'Content-Type': "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         username: usernameInput,
         password: passwordInput,
-        email: emailInput
-      })
-    })
+        email: emailInput,
+      }),
+    });
   }
   logIn(usernameInput, passwordInput) {
     fetch("http://localhost:3001/login", {
       method: "POST",
       headers: {
-        'Content-Type': "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         username: usernameInput,
-        password: passwordInput
+        password: passwordInput,
+      }),
+    })
+      .then((response) => {
+        return response.json();
       })
-    }).then(
-      response => {
-        return response.json()
-      }
-    ).then(
-      data => {
-        console.log(data)
+      .then((data) => {
         if (data.success) {
-          this.setState({session: true})
-          console.log(data.accountData)
-          localStorage.setItem("_id", data.accountData._id)
+          this.setState({ session: true });
+          localStorage.setItem("_id", data.accountData._id);
         }
-      }
-    )
+      });
   }
   logOut() {
-    localStorage.removeItem("_id")
-    this.setState({session: false})
+    localStorage.removeItem("_id");
+    this.setState({ session: false });
   }
   render() {
-    let contentPage = <LogInSignUp signUp={this.signUp} logIn={this.logIn}/>
+    let contentPage = <LogInSignUp signUp={this.signUp} logIn={this.logIn} />;
     if (this.state.session) {
-      contentPage = <GamePage logOut={this.logOut}/>
+      contentPage = <GamePage logOut={this.logOut} />;
     }
-    return(
-      <div>
-        {contentPage}
-      </div>
-    )
+    return <div>{contentPage}</div>;
   }
 }
 
